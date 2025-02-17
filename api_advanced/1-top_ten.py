e9f76e8534db8c88c0b3df34a71b71d81a988911
Seziberagabriel <g.tuyisingi@alustudent.com>
2025-02-17 10:30:38 +0200

    Task 1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
+"""
+function that queries the 'Reddit API'
+and prints the titles of the first 10 hot posts listed for a given subreddit.
+"""
 import requests
 
+
 def top_ten(subreddit):
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-    headers = {'User-Agent': 'python3:top_ten:v1.0 (by /u/yourusername)'}
-
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    url = 'https://oauth.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    if response.status_code == 200:
-        try:
-            data = response.json()
-            for post in data['data']['children']:
-                print(post['data']['title'])
-            print("OK")  # Add this to indicate the operation was successful
-        except ValueError:
-            print(None)
-    else:
+    if response.status_code != 200:
         print(None)
-
+        return
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])
